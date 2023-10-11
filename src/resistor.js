d90c6d621dcc65c3b0fc8612da54435564ba4b75
Obdulio Benitez Garcia <112982014+OBenitezG@users.noreply.github.com>
2023-10-11 19:21:56 -0400

    All tests Passing

--- src/resistor.js
+++ src/resistor.js
@@ -62,7 +62,22 @@
  * then use the copied object like a lookup table
  */
 function getColorValue(color) {
-  // write your code here & return value
+  const colorCodes = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+
+  const colorValue = colorCodes[color];
+
+  return colorValue;
 }
 
 /**
@@ -79,7 +94,24 @@ function getColorValue(color) {
  * then use the copied object like a lookup table
  */
 function getMultiplierValue(color) {
-  // write your code here & return value
+  const multiplierCodes = {
+    black: 1,
+    brown: 10,
+    red: 100,
+    orange: 1000,
+    yellow: 10000,
+    green: 100000,
+    blue: 1000000,
+    violet: 10000000,
+    grey: 100000000,
+    white: 1000000000,
+    gold: 0.1,
+    silver: 0.01,
+  };
+
+  const multiplierValue = multiplierCodes[color];
+
+  return multiplierValue;
 }
 
 /**
@@ -106,83 +138,130 @@ function getMultiplierValue(color) {
  *
  */
 function getThreeBandValue(bands) {
-  // write your code here & return value
+  const color1Value = getColorValue(bands.color1);
+  const color2Value = getColorValue(bands.color2);
+  const multiplierValue = getMultiplierValue(bands.multiplier);
+
+  let resistance = (color1Value * 10 + color2Value) * multiplierValue;
+
+  if (bands.multiplier === 'gold') {
+    resistance = Math.floor(resistance * 10) / 10;
+  } else if (bands.multiplier === 'silver') {
+    resistance = Math.floor(resistance * 100) / 100;
+  }
+
+  return resistance;
 }
 
 /**
- * Returns a string representation of the value with metric notation
- * @param {number} val - number representing the value of the resistor
- * @returns {string} - a string representation of the numeric value with metric notation
- * example: 0.26       => 0.26
- * example: 2.6        => 2.6
- * example: 26         => 26
- * example: 260        => 260
- * example: 2600       => 2.6k  // k = 1 thousand
- * example: 26000      => 26k
- * example: 260000     => 260k
- * example: 2600000    => 2.6M // M = 1 million
- * example: 26000000   => 26M
- * example: 260000000  => 260M
- * example: 2600000000 => 2.6G // G = 1 billion
- *
- *  Hint: I found a solution from stackoverflow
- * Be careful, if you copy older code make sure you replace var with
- * either const or let
- *
- */
+   * Returns a string representation of the value with metric notation
+   * @param {number} val - number representing the value of the resistor
+   * @returns {string} - a string representation of the numeric value with metric notation
+   * example: 0.26       => 0.26
+   * example: 2.6        => 2.6
+   * example: 26         => 26
+   * example: 260        => 260
+   * example: 2600       => 2.6k  // k = 1 thousand
+   * example: 26000      => 26k
+   * example: 260000     => 260k
+   * example: 2600000    => 2.6M // M = 1 million
+   * example: 26000000   => 26M
+   * example: 260000000  => 260M
+   * example: 2600000000 => 2.6G // G = 1 billion
+   *
+   *  Hint: I found a solution from stackoverflow
+   * Be careful, if you copy older code make sure you replace var with
+   * either const or let
+   *
+   */
 function formatNumber(val) {
-  // write your code here & return value
+  const metricNotation = {
+    1: '',
+    1e3: 'k',
+    1e6: 'M',
+    1e9: 'G',
+  };
+
+  const absVal = Math.abs(val);
+
+  const notation = Object.keys(metricNotation).reverse().find((i) => absVal >= i);
+
+  if (notation) {
+    const formattedVal = (val / notation).toFixed(1).replace(/\.0$/, '');
+    return formattedVal + metricNotation[notation];
+  }
+
+  return val.toString();
 }
 
 /**
- * Returns the tolerance of the resistor according to its color value
- * @param {string} color - the color of the tolerance band to
- * @returns {string} - the tolerance value in percent using the ± symbol
- *
- * Go to http://bit.ly/2NjS274 see what colors are used for the tolerance
- * lookup table.
- *
- * must create a toleranceCodes object inside this function so it is private,
- * then use the object like a lookup table
- *
- * example: 'brown' => '±1%'
- * example: 'red' => '±2%'
- * example: 'green' => '±0.5%'
- */
+   * Returns the tolerance of the resistor according to its color value
+   * @param {string} color - the color of the tolerance band to
+   * @returns {string} - the tolerance value in percent using the ± symbol
+   *
+   * Go to http://bit.ly/2NjS274 see what colors are used for the tolerance
+   * lookup table.
+   *
+   * must create a toleranceCodes object inside this function so it is private,
+   * then use the object like a lookup table
+   *
+   * example: 'brown' => '±1%'
+   * example: 'red' => '±2%'
+   * example: 'green' => '±0.5%'
+   */
 function getTolerance(color) {
-  // write your code here & return value
+  const toleranceCodes = {
+    brown: '±1%',
+    red: '±2%',
+    green: '±0.5%',
+    blue: '±0.25%',
+    violet: '±0.1%',
+    grey: '±0.05%',
+    gold: '±5%',
+    silver: '±10%',
+  };
+
+  return toleranceCodes[color];
 }
 
 /**
- *
- * @param {object} bands - the object with the 4 bands
- * @param {string} bands.color1 - the first color
- * @param {string} bands.color2 - the second color
- * @param {string} bands.multiplier - the multiplier color
- * @param {string} bands.tolerance - the tolerance color
- * @returns {string} - a string representation of the resistor value according to
- * the format in the examples
- *
- * example: {
- *   color1: 'brown',
- *   color2: 'red',
- *   multiplier: 'black',
- *   tolerance: 'brown'
- * }
- *   => '12 Ohms ±1%'
- *
- * example: {
- *   color1: 'green',
- *   color2: 'blue',
- *   multiplier: 'red',
- *   tolerance: 'grey'
- * }
- *   => 'Resistor value: 5.6k Ohms ±0.05%'
- *
- * must use functions in this file to build the string using a template literal
- */
+   *
+   * @param {object} bands - the object with the 4 bands
+   * @param {string} bands.color1 - the first color
+   * @param {string} bands.color2 - the second color
+   * @param {string} bands.multiplier - the multiplier color
+   * @param {string} bands.tolerance - the tolerance color
+   * @returns {string} - a string representation of the resistor value according to
+   * the format in the examples
+   *
+   * example: {
+   *   color1: 'brown',
+   *   color2: 'red',
+   *   multiplier: 'black',
+   *   tolerance: 'brown'
+   * }
+   *   => '12 Ohms ±1%'
+   *
+   * example: {
+   *   color1: 'green',
+   *   color2: 'blue',
+   *   multiplier: 'red',
+   *   tolerance: 'grey'
+   * }
+   *   => 'Resistor value: 5.6k Ohms ±0.05%'
+   *
+   * must use functions in this file to build the string using a template literal
+   */
 function getResistorOhms(bands) {
-  // write your code here & return value
+  const color1Value = getColorValue(bands.color1);
+  const color2Value = getColorValue(bands.color2);
+  const multiplierValue = getMultiplierValue(bands.multiplier);
+  const resistance = (color1Value * 10 + color2Value) * multiplierValue;
+  const tolerance = getTolerance(bands.tolerance);
+
+  const resistanceString = formatNumber(resistance);
+
+  return `Resistor value: ${resistanceString} Ohms ${tolerance}`;
 }
 
 module.exports = {
